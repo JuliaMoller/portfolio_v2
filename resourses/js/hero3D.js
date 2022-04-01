ffcb5d48feb82bb3b47d7357ca4d1a4e37a7ed9d
JuliaMoller <juliadmoller@gmail.com>
2022-04-01 16:23:27 +0100

    3D object - babylon - mouse interaction disable, and footer init

--- resourses/js/hero3D.js
+++ resourses/js/hero3D.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function(){
     // get the canvas DOM element
     var canvas = document.getElementById('renderCanvas');
+    //canvas.addEventListener("wheel", evt => evt.preventDefault());
 
     // load the 3D engine
     var engine = new BABYLON.Engine(canvas, true);
@@ -13,6 +14,13 @@ window.addEventListener('DOMContentLoaded', function(){
         scene.createDefaultCamera(true, false, false);
         scene.clearColor = new BABYLON.Color4(0.0, 0.0, 0.0, 0.0);
 
+        // var camera = new BABYLON.FreeCamera("sceneCamera", new BABYLON.Vector3(0, 1, -15), scene);
+
+
+        //scene.activeCamera.setTarget(BABYLON.Vector3.Zero());
+        //  camera.inputs.attached.mouse.detachControl();
+        //  camera.inputs.addGamepad();
+
         BABYLON.SceneLoader.Append("resourses/assets/", "hero3D.glb", scene, function(scene) {
             console.log(scene.meshes);
             scene.meshes[1].showBoundingBox = false;
@@ -24,10 +32,12 @@ window.addEventListener('DOMContentLoaded', function(){
             // Sets the Camera
             scene.createDefaultCameraOrLight(true, true, true);
             scene.activeCamera.setTarget(BABYLON.Vector3.Zero());
-            scene.activeCamera.attachControl(canvas, false);
+            // scene.activeCamera.attachControl(canvas, true);
+            scene.activeCamera.detachControl();
             scene.activeCamera.position = new BABYLON.Vector3(center.x, center.y, bsphere.radiusWorld * 15);
             scene.activeCamera.target = center;
-            scene.activeCamera.alpha = Math.PI / 2;                    
+            scene.activeCamera.alpha = Math.PI / 2;
+                            
         });
 
         return scene;
---
+++ resourses/js/renderCanvas.js
@@ -0,0 +1,5 @@
+canvas = document.getElementById('renderCanvas');
+ 
+    // Set canvas dimensions
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
